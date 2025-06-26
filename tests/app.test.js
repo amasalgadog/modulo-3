58315dfc0c357409b9d3f993338628d47aa951c5
gato4d <alvaro.asalgado@ug.uchile.cl>
2025-06-25 23:18:56 -0400

    se agrega archivo app.test.js como prueba unitaria

---
+++ tests/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /api/productos', () => {
+    it('debe retornar una lista de productos', async () => {
+        const response = await request(app).get('/api/productos');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.length).toBreGreaterThan(0);
+        expect(response.body[0]).toHaveProperty('nombre');
+        expect(response.body[0]).toHaveProperty('precio');
+        expect(response.body[0]).toHaveProperty('fecha_vencimiento');
+    });
+});
