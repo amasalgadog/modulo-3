9ff7e876c48275bc988eec68897f2fcce46b149b
gato4d <alvaro.asalgado@ug.uchile.cl>
2025-06-27 00:08:23 -0400

    se corrige la funcion toBeGreaterThan() en app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,7 +5,7 @@ describe('GET /api/productos', () => {
     it('debe retornar una lista de productos', async () => {
         const response = await request(app).get('/api/productos');
         expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBreGreaterThan(0);
+        expect(response.body.length).toBeGreaterThan(0);
         expect(response.body[0]).toHaveProperty('nombre');
         expect(response.body[0]).toHaveProperty('precio');
         expect(response.body[0]).toHaveProperty('fecha_vencimiento');
